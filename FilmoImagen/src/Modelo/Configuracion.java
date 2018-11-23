ad993d0be2ede271f79ad8e7467ba83f7d81e1ae
JACIEL <JACIEL@JAZZ>
2018-11-22 21:35:21 -0600

    creacion de la clase configuracion

---
+++ FilmoImagen/src/Modelo/Configuracion.java
@@ -0,0 +1,15 @@
+
+package Modelo;
+
+/**
+ *
+ * @author JACIEL
+ */
+public class Configuracion {
+    
+    protected final String URL="jdbc:mysql://localhost/cine";
+    protected final String DRIVER="com.mysql.jdbc.Driver";
+    protected final String usuario="root";
+    protected final String password="root1234";
+    
+}
